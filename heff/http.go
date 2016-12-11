6a94b58990aef5fd11bc868b8bf5c2f9a0b53e6d
Carl Johnson <me@carlmjohnson.net>
2016-12-11 10:10:04 -0500

    Handler: Use a buffer pool

--- heff/http.go
+++ heff/http.go
@@ -4,11 +4,28 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 )
+
+var pool sync.Pool
+
+func getBuffer() []byte {
+	x := pool.Get()
+	if buf, ok := x.([]byte); ok {
+		return buf
+	} else {
+		return make([]byte, 100*1<<10)
+	}
+}
+
+func putBuffer(buf []byte) {
+	pool.Put(buf)
+}
 
 func Honeypot(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Started writing: %v", r.URL)
-	buf := make([]byte, 100*1<<10)
+	buf := getBuffer()
+	defer putBuffer(buf)
 	io.WriteString(w, "<HTML>\n<BODY>\n")
 	n, err := io.CopyBuffer(w, DefaultMarkovMap, buf)
 	log.Printf("Wrote: %d (%v)", n, err)
