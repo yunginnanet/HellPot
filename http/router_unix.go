9b02101f71b8eaf0e2b88f3f582201d729965ab3
kayos@tcp.direct <kayos@tcp.direct>
2021-09-21 23:47:32 -0700

    fix windows support

---
+++ http/router_unix.go
@@ -0,0 +1,37 @@
+//+build linux darwin freebsd
+
+package http
+
+import (
+	"net"
+	"os"
+	"syscall"
+
+	"github.com/fasthttp/router"
+	"github.com/valyala/fasthttp"
+
+	"github.com/yunginnanet/HellPot/config"
+)
+
+func listenOnUnixSocket(addr string, r *router.Router) error {
+	var err error
+	var unixAddr *net.UnixAddr
+	var unixListener *net.UnixListener
+	unixAddr, err = net.ResolveUnixAddr("unix", addr)
+	if err == nil {
+		// Always unlink sockets before listening on them
+		_ = syscall.Unlink(addr)
+		// Before we set socket permissions, we want to make sure only the user HellPot is running under
+		// has permission to the socket.
+		oldmask := syscall.Umask(0077)
+		unixListener, err = net.ListenUnix("unix", unixAddr)
+		syscall.Umask(oldmask)
+		if err == nil {
+			err = os.Chmod(unixAddr.Name, os.FileMode(config.UnixSocketPermissions))
+			if err == nil {
+				err = fasthttp.Serve(unixListener, r.Handler)
+			}
+		}
+	}
+	return err
+}
---
+++ http/router_windows.go
@@ -0,0 +1,13 @@
+//go:build windows
+// +build windows
+
+package http
+
+import (
+	"errors"
+	"github.com/fasthttp/router"
+)
+
+func listenOnUnixSocket(addr string, r *router.Router) error {
+	return errors.New("unix sockets are not supported on Windows")
+}
